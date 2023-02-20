6649f92b3fd117f050da7c0d1ebf9b3bbb42ab73
Nazar-Kulyk <nazarkulyk1993@gmail.com>
2023-02-20 23:11:27 +0100

    task-07-08-9-1

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
+const body = document.querySelector("body");
+const colorSpan = document.querySelector(".color");
+const changeColorBtn = document.querySelector(".change-color");
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+changeColorBtn.addEventListener("click", () => {
+  const color = getRandomHexColor();
+  body.style.backgroundColor = color;
+  colorSpan.textContent = color;
+});
